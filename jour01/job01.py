87502663ec2b36d99c62f6cf99eb265bcb7c456f
tom-clavis <tom.clavis@laplateforme.io>
2024-01-02 09:38:54 +0100

    Job01 et Job02

---
+++ jour01/job01.py
@@ -0,0 +1,10 @@
+class operation :
+    def __init__(self, nombre1=12, nombre2=13) :
+        self.nombre1 = nombre1
+        self.nombre2 = nombre2
+        
+operation_instance = operation()
+
+print("LeNombre1 est:", operation_instance.nombre1)
+print("LeNombre2 est:", operation_instance.nombre2)
+
