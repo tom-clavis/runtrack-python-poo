d602717df3998eb9a50a7b62b8866c8b11b79400
tom-clavis <tom.clavis@laplateforme.io>
2024-01-02 14:03:13 +0100

    zizou

---
+++ jour01/job07.py
@@ -0,0 +1,20 @@
+class Personnage :
+    
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def gauche(self):
+        self.x -= 1
+
+    def droite(self):
+        self.x += 1
+
+    def haut(self):
+        self.y -= 1
+
+    def bas(self):
+        self.y += 1
+
+    def position(self):
+        return (self.x, self.y)
