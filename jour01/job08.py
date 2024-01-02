b986b2468403bf511b108e4e02130c30fea94d6c
tom-clavis <tom.clavis@laplateforme.io>
2024-01-02 14:58:48 +0100

    Update job08.py

--- jour01/job08.py
+++ jour01/job08.py
@@ -1,5 +1,34 @@
-import math
+import math 
 
-class Cercle :
-    def __init__(self) : 
-        
+class Cercle:
+    def __init__(self, rayon):
+        self.rayon = rayon              
+
+    def changerRayon(self, nouveau_rayon):
+        self.rayon = nouveau_rayon 
+    
+    def afficherInfos(self):
+        print(f"Rayon = {self.rayon}")
+        print(f"Circonférence : {self.circonference()}")
+        print(f"Diamètre : {self.diametre()}")
+        print(f"Aire : {self.aire()}")
+
+    def circonference(self):
+        return 2 * math.pi * self.rayon
+    
+    def aire(self):
+        return math.pi * self.rayon ** 2
+    
+    def diametre(self):
+        return 2 * self.rayon
+
+cercle1 = Cercle(4)
+cercle2 = Cercle(7)
+
+print("Cercle 1 : ")
+cercle1.afficherInfos()
+
+print("")
+
+print("Cercle 2 :")
+cercle2.afficherInfos()
