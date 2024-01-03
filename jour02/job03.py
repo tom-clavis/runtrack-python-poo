6e0b6d0c497eb60826765494aad82faccdbba461
tom-clavis <tom.clavis@laplateforme.io>
2024-01-03 10:34:39 +0100

    Update job03.py

--- jour02/job03.py
+++ jour02/job03.py
@@ -1,8 +1,9 @@
 class Livre:
-    def __init__(self,titre,auteur,pages):
+    def __init__(self,titre,auteur,pages,):
         self.__titre = titre
         self.__auteur = auteur
         self.__pages = pages
+        self.__disponible = True
         
     def get_titre(self):
         return self.__titre
@@ -12,6 +13,9 @@ class Livre:
     
     def get_pages(self):
         return self.__pages
+    
+    def get_disponible(self):
+        return self.__disponible
     
     def set_titre(self,titre):
         self.__titre = titre
@@ -24,3 +28,32 @@ class Livre:
             self.__pages = pages
         else:
             print("Erreur : Le nombre de pages n'est pas positif ou entier.")
+            
+    def verification(self):
+        return self.__disponible
+    
+    def emprunter(self):
+        if self.verification():
+            print("Le livre est disponible. Emprunt en cours.")
+            self.__disponible = False
+        else:
+            print("Le livre n'est plus disponible.")
+            
+    def rendre(self):
+        if not self.verification():
+            print("Le livre a bien été emprunté. Rendu en cours.")
+            self.__disponible = True
+        else:
+            print("Le livre n'a pas été emprunté.")
+            
+mon_livre = Livre("Titeuf", "Zep", 40)
+
+print("Le livre est disponible :", mon_livre.verification())
+
+mon_livre.emprunter()
+
+print("Le livre est disponible :", mon_livre.verification())
+
+mon_livre.rendre()
+
+print("Le livre est disponible :", mon_livre.verification())
